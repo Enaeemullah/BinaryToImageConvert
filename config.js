65297e20f864769943be7a858f14a33d936bb858
Naeem Ullah <enaeemullah@gmail.com>
2023-12-19 18:51:58 +0500

    Updated Sql Warning

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  server: process.env.DB_SERVER
 };
 
 async function connectToDatabase() {
